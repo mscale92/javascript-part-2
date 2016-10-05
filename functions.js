a572bbd4e59c84415ba21cc88de221b9c9cad446
Mary Scale <scalemary@gmail.com>
2016-10-05 20:52:43 +0000

    The last functions' function, returning a function within a function

--- functions.js
+++ functions.js
@@ -63,3 +63,70 @@ function squareArrays(numArr){
     return numArr.map(square);  
 };
 squareArrays([4, 16, 9, 12]);
+
+
+
+//Use map to run a square function that squares each num value in an object in an array
+
+function Number(num){
+    this.num = num;
+}
+//created a Number constructor with one key: num, and one value: num
+
+var number1 = new Number(9);
+var number2 = new Number(5);
+//Make new numbers here
+
+var numObjects = [number1, number2];
+//Put numbers into the array numObjects
+
+
+function square(obj){
+    for(x in obj){
+        var squareMe = obj[x];
+        var answer = squareMe * squareMe;
+        return answer;
+    }
+};
+//square function takes the obj in an array and squares its key values
+
+function multi2(obj){
+    for(x in obj){
+        var times = obj[x];
+        times = times * 2;
+        return times;
+    }
+};
+//multil2 function multiplies obj key values by two
+
+numObjects.map(square);
+//using the map method with our square function we can find the square of every object in the array.
+
+
+//Write a function that returns a function that takes two properties and returns the arithmatic value of the first function's input string.
+
+function operationMaker(operation){
+    return function arithma(num1, num2){
+        if(operation === "add"){
+            return num1 + num2;
+        }
+        
+        else if(operation === "sub"){
+            return num1 - num2;
+        }
+            
+        else if(operation === "multi"){
+            return num1 * num2;
+        }
+            
+        else if(operation === "div"){
+            return num1 / num2;
+        }
+        else{
+            console.log("Sorry, I only respond to 'add', 'sub', 'multi', and 'div'. Please try again");
+        }
+    }
+};
+
+var multiply = operationMaker("multi");
+var product = multiply(4,3);
