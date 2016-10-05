143ded6380927e679471edefc287cd9b416be0c9
Mary Scale <scalemary@gmail.com>
2016-10-05 18:13:23 +0000

    Array sort function

--- functions.js
+++ functions.js
@@ -1,1 +1,19 @@
-return "hello world";
+//Recursion practice
+var calcrepeatAdd = function(num){
+    if(num === 0){
+        return 0;
+    }
+    return num + calcrepeatAdd(num -1);
+};
+
+calcrepeatAdd(8);
+
+
+//Sorting function
+function stringLength(str1, str2){
+    return str1.length - str2.length;
+};
+stringLength("hello","hello world");
+
+var arrayOfStrings = ["howdy yall", "It's a me!", "hyahh","hello world, how are you?", "I'm so glad!"];
+arrayOfStrings.sort(stringLength);
